d606092fb07d0cc18f9580c43837135836da631f
Vasanthr23 <vasanth.rengaraj@softsuave.com>
2023-01-23 18:02:18 +0530

    addproduct
    addproduct

---
+++ ProductDetails/product.js
@@ -0,0 +1,67 @@
+module.exports = (db) => {
+  return {
+    CreateProduct: (req, res) => {
+      let value = [
+        req.body.ProductName,
+        req.body.CategoryId,
+        req.body.quantity,
+        req.body.Price,
+        req.body.DiscountId,
+        req.body.Description
+      ];
+            db.query(
+               ` INSERT INTO product (ProductName,CategoryId,quantity,Price,DiscountId,Description) values(?,?,?,?,?,?)`,
+              value,
+              (err, data) => {
+                if (err) {
+                  console.log(err), res.status(400).send(err);
+                } else {                  
+                  let resp = {
+                    Result: "Producted added successfully",
+                  };
+                  res.status(200).send(resp);
+                }
+              }
+            );
+          
+        
+    },
+    getProduct: (req, res) => {
+                 db.query(
+               ` SELECT ProductName,Price,quantity,Description FROM product`,
+              (err, data) => {
+                if (err) {
+                  console.log(err), res.status(400).send(err);
+                } else {                  
+                  let resp = {
+                    Result: data,
+                    msg:"Get producet successfully"
+                  };
+                  res.status(200).send(resp);
+                }
+              }
+            );
+          
+        
+    },
+   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  };
+};
--- router.js
+++ router.js
@@ -2,11 +2,8 @@
 module.exports = (app,db) => {
     
     const customers = require('./Userdetails/customers')(db);
-
     const Login = require('./Loginusers/Customerlogin')(db);
-
     const myorders = require('./OrdersItems/Order')(db);
-
     const verifyToken = require("./middleware/auth");
 
     app.post('/Createuser', customers.CreateCustomer);
@@ -18,5 +15,11 @@ module.exports = (app,db) => {
 
 
     app.post('/order',verifyToken, myorders.myorder);
-   
+
+
+
+    const myproduct = require('./ProductDetails/product')(db);
+
+    app.post('/CreateProduct', myproduct.CreateProduct);
+    app.get('/getProduct', myproduct.getProduct);
 }
